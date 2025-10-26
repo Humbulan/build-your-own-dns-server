eb2892dd34d76603bd5e766c94ff33752d446be0
Humbulani <getfriendhumbulani30@gmail.com>
2025-10-26 23:16:53 +0200

    Completed DNS server implementation - 8/8 stages

---
+++ your_program.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# Use this script to run your program.
+# This script is used by CodeCrafters to execute your program.
+# 
+# You don't need to modify this script.
+
+exec python3 -m app.main "$@"
